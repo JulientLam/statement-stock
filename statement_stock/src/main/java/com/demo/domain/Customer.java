de40bf787072f6368f37e19683e52108dddd7027
Huong <lamhuong97.utt@gmail.com>
2020-09-01 14:18:45 +0700

    [CREATE] create domain class

---
+++ statement_stock/src/main/java/com/demo/domain/Customer.java
@@ -0,0 +1,27 @@
+package com.demo.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+class Customer {
+	private long id = -1l;
+	private String firstName;
+	private String lastName;
+	private Address address;
+	private Account account;
+	private String taxId;
+
+	@Override
+	public String toString() {
+		String output = "Customer number " + id + ", " + firstName + " " + lastName;
+		if (address != null) {
+			output = output + " who lives in " + address.getCity() + "," + address.getState();
+		}
+		if (account != null && account.getTransactions() != null) {
+			output = output + " has " + account.getTransactions().size() + " transactions.";
+		}
+		return output;
+	}
+}
